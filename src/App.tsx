8726a66444409d2ee4a6605618d21af50d2ad5b2
ssl <ssl@sslui-MacBookPro.local>
2022-12-20 13:59:55 +0900

    init tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="text-3xl font-bold underline">Hello world</div>;
 }
 
 export default App;
